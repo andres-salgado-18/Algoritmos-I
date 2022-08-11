09ab47dca1aeb8b8f930543b7a855cfab61318da
Andrés Salgado <andres.salgado.1812@gmail.com>
2022-08-10 23:04:13 -0300

    Subir labo 6

---
+++ Laboratorios/Labo 6 - Testing/src/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/AFSP/Documents/University/Subjects/2º C/Algo I/LABS/Labo 6/src/lib/googletest-master/googletest
+# Build directory: C:/Users/AFSP/Documents/University/Subjects/2º C/Algo I/LABS/Labo 6/src/cmake-build-debug/lib/googletest-master/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
