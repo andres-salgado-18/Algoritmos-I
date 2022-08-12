492cc2dc1e9c4447f227ebeb55b4733df96d9ce1
Andrés Salgado <andres.salgado.1812@gmail.com>
2022-08-11 23:55:12 -0300

    Actualización labo 6

--- Laboratorios/Labo 6 - Testing/src/ej1/Test/puntajeTEST.cpp
+++ Laboratorios/Labo 6 - Testing/src/ej1/Test/puntajeTEST.cpp
@@ -2,3 +2,31 @@
 #include "gtest/gtest.h"
 
 // Escribir tests aca:
+TEST(puntajeTest, bMenor10ynoDiv3){
+    int b = 7;
+    int resExpected = 2*b - 10;
+    int result = puntaje(b);
+    EXPECT_EQ(resExpected,result);
+}
+
+TEST(puntajeTest, bMenor10yDiv3){
+    int b = 9;
+    int resExpected = 2*b + 10;
+    int result = puntaje(b);
+    EXPECT_EQ(resExpected,result);
+}
+
+TEST(puntajeTest, bMayor10ynoDiv3){
+    int b = 11;
+    int resExpected = b - 10;
+    int result = puntaje(b);
+    EXPECT_EQ(resExpected,result);
+}
+
+TEST(puntajeTest, bMayor10yDiv3){
+    int b = 12;
+    int resExpected = b + 10;
+    int result = puntaje(b);
+    EXPECT_EQ(resExpected,result);
+}
+
